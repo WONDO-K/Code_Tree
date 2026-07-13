2b63deaea0f1ccdd3a20a8cb13f1a4994372b9b0
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2026-07-13 22:31:53 +0900

    [Passed] 변수 출력하기 3 (47ms, 16MB)

--- trail0/변수 출력하기 3/outputing-variables-3.py
+++ trail0/변수 출력하기 3/outputing-variables-3.py
@@ -1,11 +1,3 @@
 a,b,c = 1,2,'C'
 
-temp = [a,b,c]
-leng = 1;
-for i in temp :
-    print(str(i), end="")
-    if leng == len(temp):
-        break
-    print("->", end="")
-    leng+=1
-
+print(a,b,c,sep="->")
