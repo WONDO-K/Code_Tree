d465026cf606a06cba7fd1c407e4939ba389f542
Dongho-Choi <90016509+WONDO-K@users.noreply.github.com>
2026-07-23 22:35:14 +0900

    [Wrong Answer] 실수 받아 그대로 출력 (164ms, 11MB)

---
+++ trail0/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        Double N = sc.nextDouble();
+        System.out.println(Math.round(N*100)/100.0);
+
+    }
+}
